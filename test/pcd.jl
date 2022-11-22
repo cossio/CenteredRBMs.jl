eb2e9ed968f4ddc4e14be8b981c2effb6cc9f5ff
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-22 23:10:38 +0100

    fix tests

--- test/pcd.jl
+++ test/pcd.jl
@@ -17,8 +17,9 @@ using RestrictedBoltzmannMachines: initialize!, mean_h_from_v, inputs_h_from_v, 
     @test rbm.offset_h ≈ vec(mean(train_h; dims=2))
     @test norm(mean(inputs_h_from_v(rbm, train_x); dims=2)) < 1e-6
 
-    pcd!(rbm, train_x; batchsize=128)
+    pcd!(rbm, train_x; batchsize=1024, iters=100)
     @test rbm.offset_v ≈ dropdims(mean(train_x; dims=3); dims=3)
     train_h = mean_h_from_v(rbm, train_x)
-    @test rbm.offset_h ≈ vec(mean(train_h; dims=2))
+    @test rbm.offset_h ≈ vec(mean(train_h; dims=2)) rtol=0.1
+    # not exact because offset is updated after having updated the parameters!
 end
