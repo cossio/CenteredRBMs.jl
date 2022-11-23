b02b1810a5b7b57098e2d7ed6e8bc9f065905a28
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-23 02:30:00 +0100

    ups

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,4 @@ module LayersTest include("layers.jl") end
 module BinaryTest include("binary.jl") end
 module PcdTest include("pcd.jl") end
 module PottsTest include("potts.jl") end
-module FromGradTest include("from_grad.jl")
+module FromGradTest include("from_grad.jl") end
